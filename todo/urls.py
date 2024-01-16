a3da552789ad765842db8ea8baa09092a4fbf3b5
N1ks48 <serega2000973@gmail.com>
2024-01-16 17:08:20 +0200

    Maps1.0

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.map_view, name='map_view'),
+    path('mapsfilter/', views.maps_filter, name='maps_filter'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import cx_Oracle
+from django.http import JsonResponse
 
 
 def map_view(request):
     try:
         connection = cx_Oracle.connect("NSAR", "L:FYUJAHB", "192.168.201.1:1521/Primus", encoding="UTF-8")
+
         sql = '''select NAME, MAP_LAT, MAP_LNG from NAU_TEST.FIRMS f
         left join (select f.id firmid, case when tc.isclosed = 1 then 7 else f.statusaptekaid end statusaptekaid
         from NAU_TEST.FIRMS f left join nau_test.firms_temprory_closed tc on tc.firmsid = f.id) st on st.firmid = f.id
         where st.STATUSAPTEKAID = 4'''
+
         cursor = connection.cursor()
         cursor.execute(sql)
         rows = cursor.fetchall()
@@ -16,15 +19,78 @@ def map_view(request):
         for row in rows:
             name, latitude, longitude = row
             locations.append({"name": name, "latitude": latitude, "longitude": longitude})
-
+        cursor.close()
+        connection.close()
     except cx_Oracle.Error as error:
         # Обработка ошибок подключения к базе данных
         print(f"Error connecting to Oracle Database: {error}")
-        locations = []  # Используйте пустой список или другое значение по умолчанию
-        # locations = [
-        #     {"name": "Киев", "latitude": 50.4501, "longitude": 30.5234},
-        #     {"name": "Львов", "latitude": 49.8429, "longitude": 24.0315},
-        #     {"name": "Одесса", "latitude": 46.4694, "longitude": 30.7409},
-        #     # Добавьте другие местоположения по мере необходимости
-        # ]
+        locations = []
+
     return render(request, 'mymap/map.html', {'locations': locations})
+
+
+def maps_filter(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        city = request.POST.get('city')
+        okpo = request.POST.get('okpo')
+
+        filters = {}
+        if name:
+            filters['NAME'] = name
+        if city:
+            filters['CITY'] = city
+        if okpo:
+            filters['OKPO'] = okpo
+
+        if filters:
+            if name or city:
+                conditions = []
+                for key, value in filters.items():
+                    if value:
+                        conditions.append(f"upper({key}) like upper('%{value}%')")
+                try:
+                    connection = cx_Oracle.connect("NSAR", "L:FYUJAHB", "192.168.201.1:1521/Primus", encoding="UTF-8")
+                    cursor = connection.cursor()
+
+                    qwr = f'''select NAME, MAP_LAT, MAP_LNG from NAU_TEST.FIRMS f
+                    left join (select f.id firmid, case when tc.isclosed = 1 then 7 else f.statusaptekaid end statusaptekaid
+                    from NAU_TEST.FIRMS f left join nau_test.firms_temprory_closed tc on tc.firmsid = f.id) st on st.firmid = f.id
+                    where st.STATUSAPTEKAID = 4 and {' AND '.join(conditions)} '''
+                    print(qwr)
+
+                    cursor.execute(qwr)
+                    results = cursor.fetchall()
+                    locations = []
+                    for result in results:
+                        name, latitude, longitude = result
+                        locations.append({"name": name, "latitude": latitude, "longitude": longitude})
+                    cursor.close()
+                    connection.close()
+                    return render(request, 'mymap/map.html', {'locations': locations})
+                except Exception as e:
+                    return JsonResponse({"error": str(e)}, status=500)
+            elif okpo:
+                try:
+                    connection = cx_Oracle.connect("NSAR", "L:FYUJAHB", "192.168.201.1:1521/Primus", encoding="UTF-8")
+                    cursor = connection.cursor()
+
+                    qwr = f'''select NAME, MAP_LAT, MAP_LNG from NAU_TEST.FIRMS f
+                    left join (select f.id firmid, case when tc.isclosed = 1 then 7 else f.statusaptekaid end statusaptekaid
+                    from NAU_TEST.FIRMS f left join nau_test.firms_temprory_closed tc on tc.firmsid = f.id) st on st.firmid = f.id
+                    where st.STATUSAPTEKAID = 4 and parentid in (SELECT id FROM NAU_TEST.FIRMS f where upper(name) like upper('%{okpo}%') and COMMENTS = 'ю') '''
+                    print(qwr)
+
+                    cursor.execute(qwr)
+                    results = cursor.fetchall()
+                    locations = []
+                    for result in results:
+                        name, latitude, longitude = result
+                        locations.append({"name": name, "latitude": latitude, "longitude": longitude})
+                    cursor.close()
+                    connection.close()
+                    return render(request, 'mymap/map.html', {'locations': locations})
+                except Exception as e:
+                    return JsonResponse({"error": str(e)}, status=500)
+        else:
+            return redirect('map_view')
